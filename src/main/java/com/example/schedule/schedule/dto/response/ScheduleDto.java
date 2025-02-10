608d70afaf7b252e89651594631b026dda4e49fe
dereck-jun <yjpark0805@naver.com>
2025-02-10 20:51:02 +0900

    refactor: `Schedule` & `ScheduleDto` 리팩토링
    - `Schedule`: Comment에 cascade 및 orphanRemoval 적용하도록 수정
    - `ScheduleDto`: 불필요한 내용 제거하고 필요한 내용만 리턴할 수 있도록 수정

--- src/main/java/com/example/schedule/schedule/dto/response/ScheduleDto.java
+++ src/main/java/com/example/schedule/schedule/dto/response/ScheduleDto.java
@@ -1,7 +1,6 @@
 package com.example.schedule.schedule.dto.response;
 
 import com.example.schedule.schedule.entity.Schedule;
-import com.example.schedule.user.dto.response.UserDto;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -14,18 +13,18 @@ public class ScheduleDto {
     private final Long scheduleId;
     private final String title;
     private final String body;
-    private final UserDto userDto;
     private final LocalDateTime createdDateTime;
     private final LocalDateTime updatedDateTime;
+    private final String username;
 
     public static ScheduleDto from(Schedule schedule) {
         return new ScheduleDto(
             schedule.getId(),
             schedule.getTitle(),
             schedule.getBody(),
-            UserDto.from(schedule.getUser()),
             schedule.getCreatedDateTime(),
-            schedule.getUpdatedDateTime()
+            schedule.getUpdatedDateTime(),
+            schedule.getUser().getUsername()
         );
     }
 }
