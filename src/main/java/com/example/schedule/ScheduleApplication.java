4040e8c41eb78136962f8307ae1d45981f3dc729
dereck-jun <yjpark0805@naver.com>
2025-02-10 14:14:52 +0900

    Initial Commit

---
+++ src/main/java/com/example/schedule/ScheduleApplication.java
@@ -0,0 +1,19 @@
+package com.example.schedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
+
+@EnableJpaAuditing
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
+@SpringBootApplication
+public class ScheduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleApplication.class, args);
+    }
+
+}
